abdc77e1404d2034ba06d142a17d01b1cdf4d898
Kfir <kfirprods@gmail.com>
2021-09-18 20:39:09 +0300

    corrected path

--- deploy-scripts/deploy-auth.sh
+++ deploy-scripts/deploy-auth.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 gcloud functions \
   deploy "maletro-auth" \
-  --source="./cloud-functions/auth" \
+  --source="${DIR}/cloud-functions/auth" \
   --runtime=nodejs14 \
   --trigger-http \
   --allow-unauthenticated
