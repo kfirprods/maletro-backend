63a1c725fb710dc824a92501ab2af08d4ed25523
Kfir <kfirprods@gmail.com>
2021-09-18 20:45:23 +0300

    dror

--- deploy-scripts/deploy-auth.sh
+++ deploy-scripts/deploy-auth.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 gcloud functions \
   deploy "maletro-auth" \
-  --source="${DIR}/cloud-functions/auth" \
+  --source="${DIR}/../cloud-functions/auth" \
   --runtime=nodejs14 \
   --trigger-http \
   --allow-unauthenticated
